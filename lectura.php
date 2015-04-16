a68c1f5eac6d6edcfcceef105803ab933e1a654b
Jonathan Guerrero <jonathan@practisis.com>
2015-04-16 16:48:56 -0500

    cambios css
    css generales

--- lectura.php
+++ lectura.php
@@ -1,21 +1,36 @@
 <!DOCTYPE html>
-<html style="background-color:#5D5D5D;">
+<html>
 	<head>
 		<meta charset="UTF-8">
 		<title>Validacion de TICKETS</title>
 		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.css" />
 		<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
 		<script src="http://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.js"></script>
+		<link rel="stylesheet" href="css/style.css">
 	</head>
 	<body>
-		<div style="background-color:#282B2C; text-align:center; padding-bottom:40px;">
-			<img src="img/ticketblanco.png" style="width:100%; max-width:300px;"/>
+		<div id="ocultar" style="background-color:#282B2C; text-align:center; padding-bottom:40px;">
+			<div style="padding-top:30px;">
+				<img src="img/ticketblanco.png" style="width:100%; max-width:300px;"/>
+			</div>
 			<br>
-			<div style="margin:30px;">
-				<input type="text" id="codigo" name="codigo" />
-				<input type="submit" value="Enviar" id="enviar" onclick="enviarDatos()" />
+			<div style="margin:30px 60px; border:2px solid #00ADEF; padding:40px;">
+				<div style="background-color:#00ADEF; padding:20px; color:#fff; margin: 0 -42px 0 20px; position:relative;">
+					<input type="text" id="codigo" name="codigo" style="background:#00709B; border:1px solid #fff;"/>
+					<input type="submit" value="ENVIAR" id="enviar" class=".btndegradate" onclick="enviarDatos()" />
+					<div class="tra_azul"></div>
+					<div class="par_azul"></div>
+				</div>
 			</div>
 		</div>
+		<div id="ok" style="margin:30px; color:#fff; font-size:50px; text-align:center; display:none;">
+			<strong>BOLETO CORRECTO</strong>
+		</div>
+		<div id="ya" style="margin:30px; color:#fff; font-size:50px; text-align:center; display:none;">
+			<strong>BOLETO YA USADO</strong>
+		</div>
+		<div id="error" style="margin:30px; color:#fff; font-size:50px; text-align:center; display:none;">
+			<strong>BOLETO INCORRECTO</strong>
 		</div>
 	</body>
 </html>
@@ -29,20 +44,25 @@ function enviarDatos(){
 		codigo : codigo
 	}).done(function(data){
 		if($.trim(data)=='Boleto Incorrecto'){
-			alert(data);
-			// $('#contenedorGlobal').load('home.html');
+			// alert(data);
+			$('#error').fadeIn('slow');
+			$('#error').delay(1000).fadeOut('slow');
+			$('#codigo').val('');
+			$('#codigo').focus();
 		}else{
 			if($.trim(data)=='Boleto Valido'){
-			alert(data);
-			// $('#inicio').css('display','block');
-			// $('#home').css('display','none');
-			// window.location='http://lcodigo.com/login/index.html';
+				// alert(data);
+				$('#ok').fadeIn('slow');
+				$('#ok').delay(1000).fadeOut('slow');
+				$('#codigo').val('');
+				$('#codigo').focus();
 			}else{
 				if($.trim(data)=='Boleto Ya Usado'){
-					alert(data);
-					// $('#inicio').css('display','block');
-					// $('#home').css('display','none');
-					// window.location='http://lcodigo.com/login/index.html';
+					// alert(data);
+					$('#ya').fadeIn('slow');
+					$('#ya').delay(1000).fadeOut('slow');
+					$('#codigo').val('');
+					$('#codigo').focus();
 				}
 			}
 		}
